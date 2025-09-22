06baf103a7fa44612a3cbb017299714e7ddf6049
nailacahya580 <nailacahya580@gmail.com>
2025-09-22 13:45:33 +0000

    hasil tampilan login

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,49 @@
 import 'package:flutter/material.dart';
-//import 'package:myapp/column_widget.dart';
-//import 'package:myapp/row_widget.dart';
-import 'package:myapp/basic_widget.dart';
+import 'login_screen.dart';
+import 'signup_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const JustduitApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class JustduitApp extends StatelessWidget {
+  const JustduitApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Praktikum Mobile Lanjut',
+      debugShowCheckedModeBanner: false,
+      title: 'Justduit',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: const Color(0xFFF3F6F8), // abu-abu muda
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E88FF)),
+        inputDecorationTheme: InputDecorationTheme(
+          isDense: true,
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 14,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFE0E0E0)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFE0E0E0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFF1E88FF)),
+          ),
+        ),
       ),
-      home: const BasicWidget(),
+      initialRoute: '/signin',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/signin': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+      },
     );
   }
 }
